6516c30d6eae48767fb305572b64379d4a5e60f0
Victor Campolina <victor.santos.tgtecnologia@gmail.com>
2021-11-01 10:33:52 -0300

    Campo minado, Aula 1: Criando parametros de dimensao da tela

---
+++ App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import { StyleSheet, View, Text } from 'react-native'
+import params from './src/params'
+
+export default class App extends Component {
+  render() {
+    return (
+      <View>
+        <Text>Iniciando o Mines!</Text>
+        <Text>Tamanho da grade:
+          {params.getRowsAmount()}x{params.getColumnsAmount()}</Text>
+      </View>
+    )
+  }
+}
