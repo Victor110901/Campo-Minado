ad647f8734c8f761319ca8b5daf2f9c84f2826ed
Victor Campolina <victor.cs2001.vc@gmail.com>
2021-11-03 14:41:28 -0300

    Campo-Minado - Criação da bandeira grande para seleção de nivel

--- App.js
+++ App.js
@@ -4,40 +4,44 @@ import params from './src/params'
 import Field from './src/components/Field'
 
 export default class App extends Component {
-render() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.Welcome}>Iniciando o Mines!</Text>
-      <Text>Tamanho da grade:
-        {params.getRowsAmount()}x{params.getColumnsAmount()}</Text>
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.Welcome}>Iniciando o Mines!</Text>
+        <Text>Tamanho da grade:
+          {params.getRowsAmount()}x{params.getColumnsAmount()}</Text>
 
-      <Field />
-      <Field opened />
-      <Field opened nearMines={1} />
-      <Field opened nearMines={2} />
-      <Field opened nearMines={3} />
-      <Field opened nearMines={6} />
-      <Field mined />
-      <Field mined opened />
-      <Field mined opened exploded/>
-      <Field flagged />
-      <Field flagged opened />
-    </View>
-  );
-}
+        <Field />
+        <Field opened />
+        <Field opened nearMines={1} />
+        <Field opened nearMines={2} />
+        <Field opened nearMines={3} />
+        <Field opened nearMines={6} />
+        <Field mined />
+        <Field mined opened />
+        <Field mined opened exploded/>
+        <Field flagged />
+        <Field flagged opened />
+      </View>
+    );
+  }
 }
 
 
 const styles = StyleSheet.create({
-container: {
-  flex: 1,
-  justifyContent: 'center',
-  alingItems: 'center',
-  backgroundColor: '#F5FCFF',
-},
-Welcome: {
-  fontSize: 20,
-  textAlign: 'center',
-  margin: 10,
-}
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alingItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  Welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instuctions: {
+    textAlign: 'center',
+    color: '#333333'
+  }
 })
